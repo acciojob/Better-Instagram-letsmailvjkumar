6ae61608c2123a0ed308dcc77b9d43bcc0fe3063
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2023-08-04 19:47:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//your code here
+const divs = document.querySelectorAll('.image');
+
+let dragSrcElement = null;
+
+function handleDragStart(e) {
+  dragSrcElement = this;
+  e.dataTransfer.effectAllowed = 'move';
+}
+
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault();
+  }
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
+
+function handleDrop(e) {
+  if (e.stopPropagation) {
+    e.stopPropagation();
+  }
+
+  if (dragSrcElement !== this) {
+    // Swap background image URLs
+    const tempBackground = this.style.backgroundImage;
+    this.style.backgroundImage = dragSrcElement.style.backgroundImage;
+    dragSrcElement.style.backgroundImage = tempBackground;
+  }
+
+  return false;
+}
+
+divs.forEach(div => {
+  div.addEventListener('dragstart', handleDragStart, false);
+  div.addEventListener('dragover', handleDragOver, false);
+  div.addEventListener('drop', handleDrop, false);
+});
